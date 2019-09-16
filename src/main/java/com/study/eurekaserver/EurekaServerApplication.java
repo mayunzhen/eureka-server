fd8b38284d78e3d3bf6ea1fa02b3e1fc3a492683
马云珍 <mayunzhensy@chinamobile.com>
2019-09-16 15:40:04 +0800

    mark url source

--- src/main/java/com/study/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/study/eurekaserver/EurekaServerApplication.java
@@ -3,7 +3,7 @@ package com.study.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
+//https://www.cnblogs.com/songlu/p/9989366.html
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
